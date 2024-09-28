0495756db461673177f087bcef9214017c86864c
yuzusim <yuzusim@gmail.com>
2024-09-28 19:00:21 +0900

    Feat: 포스트 테이블 생성

--- src/main/java/site/metacoding/blogv3/post/Post.java
+++ src/main/java/site/metacoding/blogv3/post/Post.java
@@ -1,9 +1,11 @@
 package site.metacoding.blogv3.post;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import site.metacoding.blogv3.user.User;
 
 import java.time.LocalDateTime;
 
@@ -15,8 +17,13 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String title;
 
+    @Lob
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 
     private String thumbnailFile; //섬네일
 
@@ -26,4 +33,14 @@ public class Post {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    @Builder
+    public Post(Integer id, String title, String content, User user, String thumbnailFile, Boolean isPostOwner, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.thumbnailFile = thumbnailFile;
+        this.isPostOwner = isPostOwner;
+        this.createdAt = createdAt;
+    }
 }
